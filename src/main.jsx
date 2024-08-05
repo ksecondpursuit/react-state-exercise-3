5054a45213e31b3afe17c79157eed4c1576bfaeb
Kenny <ksecond@pursuit.org>
2024-08-05 13:55:00 -0400

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('grid')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+const handleClick = (row, col) => {
+  const newGrid = grid.map(row =>row.slice());
+  newGrid[row][col] ='X';
+  setGrid(newGrid);
+};
